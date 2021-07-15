7a62d02505d9e421cc1d0b75993b54ad650f5bfa
David Jurado <david.jurado@factored.ai>
2021-07-15 10:58:10 -0500

    Add user config handling

--- mnist/example_runner.py
+++ mnist/example_runner.py
@@ -6,6 +6,31 @@ import yaml
 from mlcube_docker.docker_run import DockerRun
 
 
+def load_config(mlcube_config_path: str, user_config_path: str):
+    """Returns dictionary containing MLCube configuration"""
+    # Load mlcube config data
+    try:
+        with open(mlcube_config_path) as stream:
+                mlcube_config_data = yaml.load(stream.read(), Loader=yaml.SafeLoader)
+    except IOError as exc:
+        # If file doesn't exist throw the exception:
+        # OSError: {PATH_TO}/mnist/mlcube.yaml: No such file or directory
+        raise IOError("%s: %s" % (mlcube_config_path, exc.strerror))
+
+    # Load user config data if file exists
+    if os.path.isfile(user_config_path):
+        with open(user_config_path) as stream:
+                user_config_data = yaml.load(stream.read(), Loader=yaml.SafeLoader)
+    else:
+        return mlcube_config_data
+
+    # Merge config data
+    tmp = mlcube_config_data['container']
+    mlcube_config_data['container'] = user_config_data['container']
+    mlcube_config_data['container'].update(tmp)
+    return mlcube_config_data
+
+
 def main():
     """
     python example_runner.py --mlcube=PATH --platform=docker --task download|train [--workspace=PATH]
@@ -20,13 +45,16 @@ def main():
             mlcube_config.container.update(tmp)
         4. Follow the code below - run this one particular task (task).
     """
+    home = os.path.expanduser("~")
     root: str = os.path.dirname(os.path.abspath(__file__))
     workspace: str = os.path.join(root, 'workspace')  # or take from a command line
 
+    mlcube_config_path = os.path.join(root, 'mlcube.yaml')
+    user_config_path = os.path.join(home, '.mlcube.yaml')
+    mlcube_config_data = load_config(mlcube_config_path, user_config_path)
+
     for task in ('download', 'train'):
-        with open(os.path.join(root, 'mlcube.yaml')) as stream:
-            mlcube_config = yaml.load(stream.read(), Loader=yaml.SafeLoader)
-        docker_runner = DockerRun(mlcube_config, root=root, workspace=workspace, task=task)
+        docker_runner = DockerRun(mlcube_config_data, root=root, workspace=workspace, task=task)
         docker_runner.run()
 
 
