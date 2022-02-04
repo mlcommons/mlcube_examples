20beb465b7bd479f59d995ce4b653572378c508c
David Jurado <david.jurado@factored.ai>
2022-02-04 12:34:11 -0500

    Update mlcube.yaml

--- brats_challenge/implementation/run.sh
+++ brats_challenge/implementation/run.sh
@@ -2,9 +2,11 @@
 
 DATA_DIR="${PLATFORM:-}"
 OUTPUT_DIR="${OUTPUT_DIR:-}"
+MODEL_IN="${MODEL_IN:-}"
+LOG_DIR="${LOG_DIR:-}"
+METRICS="${METRICS:-}"
 MODE="${MODE:-}"
 if [ "$MODE" = "train" ]; then
-then
     python3 -m src.train --devices 0 --width 48 --arch EquiUnet
 else
     python3 -m src.inference --devices 0 --width 48 --arch EquiUnet
