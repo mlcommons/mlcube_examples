ce9a20428e07c7707ab1a2273094fc479e3050a1
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2022-02-24 18:38:18 -0500

    Implement Data Preparator template

--- medperf/data_preparator/project/mlcube.py
+++ medperf/data_preparator/project/mlcube.py
@@ -30,8 +30,8 @@ def prepare(
     params_file: str = typer.Option(..., "--parameters_file"),
     out_path: str = typer.Option(..., "--output_path")
 ):
-    """infer task command. This is what gets executed when we run:
-    `mlcube run infer`
+    """Prepare task command. This is what gets executed when we run:
+    `mlcube run --task=prepare`
 
     Args:
         data_path (str): Location of the data to transform. Required for Medperf Data Preparation MLCubes.
@@ -39,16 +39,43 @@ def prepare(
         params_file (str): Location of the parameters.yaml file. Required for Medperf Data Preparation MLCubes.
         out_path (str): Location to store transformed data. Required for Medperf Data Preparation MLCubes.
     """
-    with open(params_file, "r") as f:
-        params = yaml.safe_load(f)
-
-    num_words = params["num_words"]
-    cmd = f"python3 transform.py --data={data_path} --labels={labels_path} --num_words={num_words} --out={out_path}"
+    cmd = f"python3 prepare.py --names_path={data_path} --labels_path={labels_path} --out={out_path}"
     exec_python(cmd)
 
-@app.command("hotfix")
-def hotfix():
-    pass
+@app.command("sanity_check")
+def sanity_check(
+    data_path: str = typer.Option(..., "--data_path"), 
+    params_file: str = typer.Option(..., "--parameters_file")
+):
+    """Sanity check task command. This is what gets executed when we run:
+    `mlcube run --task=sanity_check`
+
+    Args:
+        data_path (str): Location of the prepared data. Required for Medperf Data Preparation MLCubes.
+        params_file (str): Location of the parameters.yaml file. Required for Medperf Data Preparation MLCubes.
+    """
+    cmd = f"python3 sanity_check.py --data_path={data_path}"
+    exec_python(cmd)
+
+@app.command("statistics")
+def statistics(
+    data_path: str = typer.Option(..., "--data_path"), 
+    params_file: str = typer.Option(..., "--parameters_file"),
+    output_path: str = typer.Option(..., "--output_path")
+):
+    """Computes statistics about the data. This statistics are uploaded
+    to the Medperf platform under the data owner's approval. Include
+    every statistic you consider useful for determining the nature of the
+    data, but keep in mind that we want to keep the data as private as 
+    possible.
+
+    Args:
+        data_path (str): Location of the prepared data. Required for Medperf Data Preparation MLCubes.
+        params_file (str): Location of the parameters.yaml file. Required for Medperf Data Preparation MLCubes.
+        output_path (str): File to store the statistics. Must be statistics.yaml. Required for Medperf Data Preparation MLCubes. 
+    """
+    cmd = f"python3 statistics.py --data_path={data_path} --out_file={output_path}"
+    exec_python(cmd)
 
 if __name__ == "__main__":
     app()
