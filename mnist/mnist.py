6a62547514352a5f9176f42d3fa8f7d420902657
Sergey Serebryakov <sergey.serebryakov@hpe.com>
2022-03-30 14:32:31 -0700

    Updating MNIST implementation.
    Re-designing MNIST MLCube implementation.
      - Removing references to python 2.x.
      - Unification of log messages.
      - Using pathlib.Path instead of os.path.xxx methods.
      - Adding docstrings.

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,42 +1,45 @@
-
-"""
-https://www.tensorflow.org/tutorials/quickstart/beginner
-Disable GPUs - not all nodes used for testing have GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
-"""
-
-from __future__ import (absolute_import, division, print_function, unicode_literals)
-import yaml
 import os
+import yaml
+import uuid
+import argparse
+import typing as t
 import logging
 import logging.config
-import argparse
+from pathlib import Path
 from enum import Enum
-from typing import List
 import numpy as np
 import tensorflow as tf
-from tensorflow_core.python.keras.utils.data_utils import get_file
-
 
 logger = logging.getLogger(__name__)
 
 
+def log(task: str, execution_id: str, event: str, context: t.Optional[t.Dict] = None) -> None:
+    logger.info("MLCube name=MNIST, task=%s, execution_id: %s event=%s, context='%s'",
+                task, execution_id, event, str(context))
+
+
 class Task(str, Enum):
-    DownloadData = 'download'
+    """Tasks implemented in this MLCube"""
+
+    Download = 'download'
+    """Download MNIST dataset."""
+
     Train = 'train'
+    """Train a simple neural network model"""
 
 
-def create_directory(path: str) -> None:
-    if not os.path.exists(path):
-        os.makedirs(path, exist_ok=True)
+def download(execution_id: str, task_args: t.List[str]) -> None:
+    """Download MNIST dataset.
+    Args:
+        execution_id: Identifier of this task execution.
+        task_args: Unparsed task-specific CLI arguments.
 
-
-def download(task_args: List[str]) -> None:
-    """ Task: download.
-    Input parameters:
-        --data_dir
+    MLCube:
+        Input parameters: data_config
+        Output parameters: data_dir
     """
-    logger.info(f"Starting '{Task.DownloadData}' task")
+    log(Task.Download, execution_id, 'task_started', dict(task_args=task_args))
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_config', '--data-config', type=str, default=None,
                         help="Path to a YAML file with data configuration.")
@@ -45,38 +48,29 @@ def download(task_args: List[str]) -> None:
 
     with open(args.data_config, 'r') as stream:
         data_config = yaml.load(stream, Loader=yaml.FullLoader)
-    logger.info("Data configuration has been read (%s).", args.data_config)
+    log(Task.Download, execution_id, 'configuration_read', dict(data_config=data_config))
 
-    if args.data_dir is None:
-        raise ValueError("Data directory is not specified (did you use --data-dir=PATH?)")
     os.makedirs(args.data_dir, exist_ok=True)
-
-    if not args.data_dir.startswith("/"):
-        logger.warning("Data directory seems to be a relative path.")
-
-    data_file = os.path.join(args.data_dir, 'mnist.npz')
-    if os.path.exists(data_file):
-        logger.info("MNIST data has already been download (file exists: %s)", data_file)
-        return
-
-    data_file = get_file(
-        fname=data_file,
+    data_file = tf.keras.utils.get_file(
+        fname=Path(args.data_dir) / 'mnist.npz',
         origin=data_config.get('uri', 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'),
         file_hash=data_config.get('hash', '731c5ac602752760c8e48fbffcf8c3b850d9dc2a2aedcf2cc48468fc17b673d1')
     )
-
-    if not os.path.isfile(data_file):
-        raise ValueError(f"MNIST dataset has not been downloaded - dataset file does not exist: {data_file}")
-    else:
-        logger.info("MNIST dataset has been downloaded.")
-    logger.info("The '%s' task has been completed.", Task.DownloadData)
+    log(Task.Download, execution_id, 'task_completed', dict(data_file=data_file))
 
 
-def train(task_args: List[str]) -> None:
-    """ Task: train.
-    Input parameters:  --data_dir, --train_config
-    Output parameters: --log_dir, --model_dir
+def train(execution_id: str, task_args: t.List[str]) -> None:
+    """ Train a classification neural network model.
+    Args:
+        execution_id: Identifier of this task execution.
+        task_args: Unparsed task-specific CLI arguments.
+
+    MLCube:
+        Input parameters: data_dir, train_config
+        Output parameters: log_dir, model_dir
     """
+    log(Task.Train, execution_id, 'task_started', dict(task_args=task_args))
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', '--data-dir', type=str, default=None, help="Path to training dataset.")
     parser.add_argument('--train_config', '--train-config', type=str, default=None,
@@ -86,14 +80,14 @@ def train(task_args: List[str]) -> None:
 
     with open(args.train_config, 'r') as stream:
         train_config = yaml.load(stream, Loader=yaml.FullLoader)
-    logger.info("Train configuration has been read (%s).", args.train_config)
+    log(Task.Train, execution_id, 'configuration_read', dict(train_config=train_config))
 
-    dataset_file = os.path.join(args.data_dir, 'mnist.npz')
-    with np.load(dataset_file, allow_pickle=True) as f:
+    data_file = Path(args.data_dir) / 'mnist.npz'
+    with np.load(str(data_file), allow_pickle=True) as f:
         x_train, y_train = f['x_train'], f['y_train']
         x_test, y_test = f['x_test'], f['y_test']
     x_train, x_test = x_train / 255.0, x_test / 255.0
-    logger.info("Dataset has been loaded (%s).", dataset_file)
+    log(Task.Train, execution_id, 'data_loaded', dict(data_file=data_file))
 
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -101,26 +95,25 @@ def train(task_args: List[str]) -> None:
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10, activation='softmax')
     ])
-    logger.info("Model has been built.")
-
     model.compile(
         optimizer=train_config.get('optimizer', 'adam'),
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
-    logger.info("Model has been compiled.")
+    log(Task.Train, execution_id, 'model_built')
 
     # Train and evaluate
     model.fit(x_train, y_train, batch_size=train_config.get('batch_size', 32),
               epochs=train_config.get('train_epochs', 5))
-    logger.info("Model has been trained.")
+    log(Task.Train, execution_id, 'model_trained')
 
     model.evaluate(x_test, y_test, verbose=2)
-    logger.info("Model has been evaluated.")
+    log(Task.Train, execution_id, 'model_evaluated')
 
     os.makedirs(args.model_dir, exist_ok=True)
-    model.save(os.path.join(args.model_dir, 'mnist_model'))
-    logger.info("Model has been saved.")
+    model_path = str(Path(args.model_dir) / 'mnist_model')
+    model.save(model_path)
+    log(Task.Train, execution_id, 'model_saved', dict(model_path=model_path))
 
 
 def main():
@@ -128,43 +121,42 @@ def main():
     mnist.py task task_specific_parameters...
     """
     # noinspection PyBroadException
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('mlcube_task', type=str, help="Task for this MLCube.")
-        parser.add_argument('--log_dir', '--log-dir', type=str, required=True, help="Logging directory.")
-        mlcube_args, task_args = parser.parse_known_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mlcube_task', type=str, help="Task for this MLCube.")
+    parser.add_argument('--log_dir', '--log-dir', type=str, required=True, help="Logging directory.")
+    mlcube_args, task_args = parser.parse_known_args()
 
-        os.makedirs(mlcube_args.log_dir, exist_ok=True)
-        logger_config = {
-            "version": 1,
-            "disable_existing_loggers": True,
-            "formatters": {
-                "standard": {"format": "%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s"},
-            },
-            "handlers": {
-                "file_handler": {
-                    "class": "logging.FileHandler",
-                    "level": "INFO",
-                    "formatter": "standard",
-                    "filename": os.path.join(mlcube_args.log_dir, f"mlcube_mnist_{mlcube_args.mlcube_task}.log")
-                }
-            },
-            "loggers": {
-                "": {"level": "INFO", "handlers": ["file_handler"]},
-                "__main__": {"level": "NOTSET", "propagate": "yes"},
-                "tensorflow": {"level": "NOTSET", "propagate": "yes"}
+    os.makedirs(mlcube_args.log_dir, exist_ok=True)
+    logger_config = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "standard": {"format": "%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s"},
+        },
+        "handlers": {
+            "file_handler": {
+                "class": "logging.FileHandler",
+                "level": "INFO",
+                "formatter": "standard",
+                "filename": os.path.join(mlcube_args.log_dir, f"mlcube_mnist_{mlcube_args.mlcube_task}.log")
             }
+        },
+        "loggers": {
+            "": {"level": "INFO", "handlers": ["file_handler"]},
+            "__main__": {"level": "NOTSET", "propagate": "yes"},
+            "tensorflow": {"level": "NOTSET", "propagate": "yes"}
         }
-        logging.config.dictConfig(logger_config)
+    }
+    logging.config.dictConfig(logger_config)
 
-        if mlcube_args.mlcube_task == Task.DownloadData:
-            download(task_args)
-        elif mlcube_args.mlcube_task == Task.Train:
-            train(task_args)
-        else:
-            raise ValueError(f"Unknown task: {task_args}")
-    except Exception as err:
-        logger.exception(err)
+    execution_id = str(uuid.uuid4())
+    if mlcube_args.mlcube_task == Task.Download:
+        download(execution_id, task_args)
+    elif mlcube_args.mlcube_task == Task.Train:
+        train(execution_id, task_args)
+    else:
+        raise ValueError(f"Unknown task: {task_args}")
+    print(f"MLCube task ({mlcube_args.mlcube_task}) completed. See log file for details.")
 
 
 if __name__ == '__main__':
