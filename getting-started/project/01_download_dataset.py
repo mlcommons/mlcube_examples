36ac1ae785130a77b30d0051559fc9a9695a20c4
David Jurado <david.jurado@factored.ai>
2022-09-09 11:20:56 -0500

    Add parameters.yaml

--- getting-started/project/01_download_dataset.py
+++ getting-started/project/01_download_dataset.py
@@ -1,17 +1,15 @@
 """Download the raw Boston Housing Dataset"""
 import os
+import yaml
 import argparse
 import requests
 
-DATASET_URL = "http://lib.stat.cmu.edu/datasets/boston"
-
-
-def download_dataset(data_dir):
+def download_dataset(data_dir, dataset_url):
     """Download dataset and store it in a given path.
     Args:
         data_dir (str): Dataset download path."""
 
-    request = requests.get(DATASET_URL)
+    request = requests.get(dataset_url)
     file_name = "raw_dataset.txt"
     file_path = os.path.join(data_dir, file_name)
     with open(file_path,'wb') as f:
@@ -24,10 +22,19 @@ def main():
     parser = argparse.ArgumentParser(description='Download dataset')
     parser.add_argument('--data_dir', required=True,
                         help='Dataset download path')
+    parser.add_argument('--parameters_file', required=True,
+                        help='File containing extra parameters')
     args = parser.parse_args()
 
     data_dir = args.data_dir
-    download_dataset(data_dir)
+    parameters_file = args.parameters_file
+
+    with open(parameters_file, 'r') as stream:
+        parameters = yaml.safe_load(stream)
+    
+    dataset_url = parameters["dataset_url"]
+
+    download_dataset(data_dir, dataset_url)
 
 
 if __name__ == '__main__':
--- getting-started/project/03_train.py
+++ getting-started/project/03_train.py
@@ -1,20 +1,21 @@
 """Train gradient boosting regressor on Boston housing dataset"""
-import os
+import yaml
 import argparse
+from re import I
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor
 
 
-def train(dataset_file_path):
+def train(dataset_file_path, n_estimators):
     df = pd.read_csv(dataset_file_path)
 
     data = df.drop(['PRICE'], axis=1)
     target = df[['PRICE']]
     X_train, X_test, Y_train, Y_test = train_test_split(data, target, test_size = 0.25)
 
-    clf = GradientBoostingRegressor(n_estimators=100, verbose = 1)
+    clf = GradientBoostingRegressor(n_estimators=n_estimators, verbose = 1)
     clf.fit(X_train, Y_train.values.ravel())
 
     train_predicted = clf.predict(X_train)
@@ -33,10 +34,21 @@ def main():
     parser = argparse.ArgumentParser(description='Train model')
     parser.add_argument('--dataset_file_path', required=True,
                         help='Processed dataset file path')
+    parser.add_argument('--parameters_file', required=False,
+                        help='File containing hyperparameters')
     args = parser.parse_args()
 
     dataset_file_path = args.dataset_file_path
-    train(dataset_file_path)
+    parameters_file = args.parameters_file
+    
+    n_estimators = 100
+    if parameters_file is not None:
+        with open(parameters_file, 'r') as stream:
+            parameters = yaml.safe_load(stream)
+            n_estimators = int(parameters["n_estimators"])
+
+    print(f"Using {n_estimators} estimators")
+    train(dataset_file_path, n_estimators)
 
 
 if __name__ == '__main__':
