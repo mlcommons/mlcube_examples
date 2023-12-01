518174d896cbb97dd89cb4431ee9c34d1f6c2b40
David Jurado <david.jurado@factored.ai>
2023-12-01 17:52:17 -0500

    Add NVIDIA_VISIBLE_DEVICES in output

--- gpus/check_gpus.sh
+++ gpus/check_gpus.sh
@@ -14,5 +14,6 @@ while [ $# -gt 0 ]; do
 done
 
 echo "CUDA_VISIBLE_DEVICES $CUDA_VISIBLE_DEVICES" |& tee "$LOG_DIR/gpus.log"
+echo "NVIDIA_VISIBLE_DEVICES $NVIDIA_VISIBLE_DEVICES" |& tee "$LOG_DIR/gpus.log"
 nvidia-smi |& tee -a "$LOG_DIR/gpus.log"
 nvidia-smi --query-gpu=gpu_name,uuid --format=csv |& tee -a "$LOG_DIR/gpus.log"
